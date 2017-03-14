c92045a6d3c9152206fa5710123a66df6df181a1
Gilberto Ribeiro Paz da Rosa <Gilberto Ribeiro Paz da Rosa>
2017-03-14 01:47:49 -0300

    [add] Matrix4 with orthographic projection

--- include/Matrix4.h
+++ include/Matrix4.h
@@ -111,6 +111,33 @@ public:
 
         return os;
     }
+
+    Matrix4 orthoProjection(float left, float right, float bottom, float top, float Znear, float Zfar) {
+            
+            Matrix4 orthomatrix;
+
+            orthomatrix[0].x = 2.0/(right-left);
+            orthomatrix[0].y = 0;
+            orthomatrix[0].z = 0;
+            orthomatrix[0].w = 0;
+ 
+            orthomatrix[1].x = 0;
+            orthomatrix[1].y = 2.0/(top-bottom);
+            orthomatrix[1].z = 0;
+            orthomatrix[1].w = 0;
+ 
+            orthomatrix[2].x = 0;
+            orthomatrix[2].y = 0;
+            orthomatrix[2].z = 2.0/(Zfar-Znear);
+            orthomatrix[2].w = 0;
+ 
+            orthomatrix[3].x = -(right+left)/(right-left);
+            orthomatrix[3].y = -(top+bottom)/(top-bottom);
+            orthomatrix[3].z = -(Zfar+Znear)/(Zfar-Znear);
+            orthomatrix[3].w = 1;
+
+            return orthomatrix;
+    }
 };
 
 #endif /*MATRIX4_H*/
