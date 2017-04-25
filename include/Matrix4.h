e633f15b494e93294445df13549de5cac1e0d0da
Gilberto Ribeiro Paz da Rosa <Gilberto Ribeiro Paz da Rosa>
2017-04-25 00:07:22 -0300

    [fixes]

--- include/Matrix4.h
+++ include/Matrix4.h
@@ -1,12 +1,17 @@
-#ifndef MATRIX4_H
-#define MATRIX4_H
-
 #define DEBUG
 #include "linearmath_local_definitions.h"
 #include <Debug.h>
 #include <Vector4.h>
+#include <Quaternion.h>
+
+#ifndef MATRIX4_H
+#define MATRIX4_H
+
+struct Quaternion;
 
 class Matrix4 {
+
+public:
 
     union {
         float m_1d[16];
@@ -16,8 +21,6 @@ class Matrix4 {
         #endif
         Vector4 m_vecrows[4];
     };
-
-public:
 
     inline Matrix4() {
 
@@ -69,7 +72,7 @@ public:
         return m_2d[index];
     }
 
-    inline float& operator()(int rowIdx, int colIdx) {
+    inline const float operator()(int rowIdx, int colIdx) const {
         ASSERT(rowIdx < 4 && colIdx < 4, "matrix4 access out of bounds");
 
         return m_2d[rowIdx][colIdx];
@@ -88,7 +91,40 @@ public:
         mr.m_vecrows[2] = m1.m_vecrows[2] * m2;
         mr.m_vecrows[3] = m1.m_vecrows[3] * m2;
 
+        /*
+        |  1  2  3  4 | * |  1  5  9 13 | = | (1 * 1 + 2 * 2 + 3 * 3 + 4 * 4) (1 * 5 + 2 * 6 + 3 * 7 + 4 * 8)  ...
+        |  5  6  7  8 | * |  2  6 10 14 | = |
+        |  9 10 11 12 | * |  3  7 11 15 | = |
+        | 13 14 15 16 | * |  4  8 12 16 | = |
+        */
+
+        // mr[0][0] = m1(0, 0) * m2(0, 0) + m1(0, 1) * m2(1, 0) + m1(0, 2) * m2(2, 0) + m1(0, 3) * m2(3, 0);
+        // mr[0][1] = m1(0, 0) * m2(0, 1) + m1(0, 1) * m2(1, 1) + m1(0, 2) * m2(2, 1) + m1(0, 3) * m2(3, 1);
+        // mr[0][2] = m1(0, 0) * m2(0, 2) + m1(0, 1) * m2(1, 2) + m1(0, 2) * m2(2, 2) + m1(0, 3) * m2(3, 2);
+        // mr[0][3] = m1(0, 0) * m2(0, 3) + m1(0, 1) * m2(1, 3) + m1(0, 2) * m2(2, 3) + m1(0, 3) * m2(3, 3);
+        // mr[1][0] = m1(1, 0) * m2(0, 0) + m1(1, 1) * m2(1, 0) + m1(1, 2) * m2(2, 0) + m1(1, 3) * m2(3, 0);
+        // mr[1][1] = m1(1, 0) * m2(0, 1) + m1(1, 1) * m2(1, 1) + m1(1, 2) * m2(2, 1) + m1(1, 3) * m2(3, 1);
+        // mr[1][2] = m1(1, 0) * m2(0, 2) + m1(1, 1) * m2(1, 2) + m1(1, 2) * m2(2, 2) + m1(1, 3) * m2(3, 2);
+        // mr[1][3] = m1(1, 0) * m2(0, 3) + m1(1, 1) * m2(1, 3) + m1(1, 2) * m2(2, 3) + m1(1, 3) * m2(3, 3);
+        // mr[2][0] = m1(2, 0) * m2(0, 0) + m1(2, 1) * m2(1, 0) + m1(2, 2) * m2(2, 0) + m1(2, 3) * m2(3, 0);
+        // mr[2][1] = m1(2, 0) * m2(0, 1) + m1(2, 1) * m2(1, 1) + m1(2, 2) * m2(2, 1) + m1(2, 3) * m2(3, 1);
+        // mr[2][2] = m1(2, 0) * m2(0, 2) + m1(2, 1) * m2(1, 2) + m1(2, 2) * m2(2, 2) + m1(2, 3) * m2(3, 2);
+        // mr[2][3] = m1(2, 0) * m2(0, 3) + m1(2, 1) * m2(1, 3) + m1(2, 2) * m2(2, 3) + m1(2, 3) * m2(3, 3);
+        // mr[3][0] = m1(3, 0) * m2(0, 0) + m1(3, 1) * m2(1, 0) + m1(3, 2) * m2(2, 0) + m1(3, 3) * m2(3, 0);
+        // mr[3][1] = m1(3, 0) * m2(0, 1) + m1(3, 1) * m2(1, 1) + m1(3, 2) * m2(2, 1) + m1(3, 3) * m2(3, 1);
+        // mr[3][2] = m1(3, 0) * m2(0, 2) + m1(3, 1) * m2(1, 2) + m1(3, 2) * m2(2, 2) + m1(3, 3) * m2(3, 2);
+        // mr[3][3] = m1(3, 0) * m2(0, 3) + m1(3, 1) * m2(1, 3) + m1(3, 2) * m2(2, 3) + m1(3, 3) * m2(3, 3);
         return mr;
+    }
+
+    inline Matrix4& operator*=(const Matrix4& m1) {
+
+        m_vecrows[0] = m1.m_vecrows[0] * (*this);
+        m_vecrows[1] = m1.m_vecrows[1] * (*this);
+        m_vecrows[2] = m1.m_vecrows[2] * (*this);
+        m_vecrows[3] = m1.m_vecrows[3] * (*this);
+
+        return *this;
     }
 
     inline friend Vector4 operator*(const Vector4& u, const Matrix4& m) {
@@ -107,6 +143,7 @@ public:
         
         return v;
     }
+    
 
     inline Matrix4& operator=(const Matrix4& mo) {
         m_vecrows[0] = mo.m_vecrows[0];
@@ -123,18 +160,54 @@ public:
                 m1.m_vecrows[3] == m2.m_vecrows[3]);
     }
 
-    inline void setTranslation(const Vector3& position) {
-        
-        m_2d[0][3] += position.x;
-        m_2d[1][3] += position.y;
-        m_2d[2][3] += position.z;
+    static inline Matrix4 Rotation(float angle, const Vector3& axis)
+	{
+		Matrix4 result(1.0f);
+
+		float r = ToRadians(angle);
+		float c = cos(r);
+		float s = sin(r);
+		float omc = 1.0f - c;
+		
+		float x = axis.x;
+		float y = axis.y;
+		float z = axis.z;
+
+		result.m_1d[0 + 0 * 4] = x * x * omc + c;
+		result.m_1d[0 + 1 * 4] = y * x * omc + z * s;
+		result.m_1d[0 + 2 * 4] = x * z * omc - y * s;
+
+		result.m_1d[1 + 0 * 4] = x * y * omc - z * s;
+		result.m_1d[1 + 1 * 4] = y * y * omc + c;
+		result.m_1d[1 + 2 * 4] = y * z * omc + x * s;
+
+		result.m_1d[2 + 0 * 4] = x * z * omc + y * s;
+		result.m_1d[2 + 1 * 4] = y * z * omc - x * s;
+		result.m_1d[2 + 2 * 4] = z * z * omc + c;
+		
+		return result;
+	}
+
+    static inline Matrix4 Rotation(Quaternion& q) {
+        return Matrix4::Rotation(q.s, q.v);
     }
 
-    inline void setScale(const Vector3& scale) {
+    static inline Matrix4 Translation(const Vector3& position) {
         
-        m_2d[0][0] *= scale.x;
-        m_2d[1][1] *= scale.y;
-        m_2d[2][2] *= scale.z;
+        Matrix4 m(1.0f);
+        m[0][3] = position.x;
+        m[1][3] = position.y;
+        m[2][3] = position.z;
+        return m;
+    }
+
+    static inline Matrix4 Scale(const Vector3& scale) {
+        
+        Matrix4 m(1.0f);
+        m[0][0] = scale.x;
+        m[1][1] = scale.y;
+        m[2][2] = scale.z;
+        return m;
     }
 
     inline friend std::ostream& operator<<(std::ostream& os, const Matrix4& m) {
@@ -149,11 +222,11 @@ public:
 
     inline static Matrix4 lookAt(Vector3 eye, Vector3 target, Vector3 up) {
 
-        Vector3 vz = (eye - target).normalize();
-        Vector3 vx = cross(up, vz).normalize();
+        Vector3 vz((eye - target).normalize());
+        Vector3 vx(cross(up, vz).normalize());
         // vy doesn't need to be normalized because it's a cross
         // product of 2 normalized vectors
-        Vector3 vy = cross(vz, vx);
+        Vector3 vy(cross(vz, vx));
         float matrix[] = {
             vx.x, vx.y, vx.z, 0,
             vy.x, vy.y, vy.z, 0,
@@ -166,27 +239,18 @@ public:
     }
 
     inline static Matrix4 orthoProjection(float left, float right, float bottom, float top, float Znear, float Zfar) {
-            
-            Matrix4 orthomatrix;
 
-            orthomatrix[0][0] = 2.0/(right-left);
-            orthomatrix[0][1] = 0;
-            orthomatrix[0][2] = 0;
-            orthomatrix[0][3] = 0;
+            Matrix4 orthomatrix(1.0f);
+
+            orthomatrix[0][0] = 2.0f / (right - left);
+            
+            orthomatrix[1][1] = 2.0f / (top - bottom);
+            
+            orthomatrix[2][2] = 2.0f / (Znear-Zfar);
  
-            orthomatrix[1][0] = 0;
-            orthomatrix[1][1] = 2.0/(top-bottom);
-            orthomatrix[1][2] = 0;
-            orthomatrix[1][3] = 0;
- 
-            orthomatrix[2][0] = 0;
-            orthomatrix[2][1] = 0;
-            orthomatrix[2][2] = 2.0/(Zfar-Znear);
-            orthomatrix[2][3] = 0;
- 
-            orthomatrix[3][0] = -(right+left)/(right-left);
-            orthomatrix[3][1] = -(top+bottom)/(top-bottom);
-            orthomatrix[3][2] = -(Zfar+Znear)/(Zfar-Znear);
+            orthomatrix[3][0] = (left + right) / (left - right);
+            orthomatrix[3][1] = (bottom + top) / (bottom - top);
+            orthomatrix[3][2] = (Zfar + Znear) / (Zfar - Znear);
             orthomatrix[3][3] = 1;
 
             return orthomatrix;
--- include/linearmath_local_definitions.h
+++ include/linearmath_local_definitions.h
@@ -1,7 +1,7 @@
 #ifndef DEFINITIONS_H
 #define DEFINITIONS_H
 
-#define USE_SSE 1
+//#define USE_SSE 1
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,5 +11,13 @@
 #include "SSEs.h"
 #define LF "\n"
 #define PI 3.1415
+
+// Converts degrees to radians.
+#define ToRadians(angleDegrees) ((angleDegrees) * PI / 180.0)
+
+// Converts radians to degrees.
+#define ToDegrees(angleRadians) ((angleRadians) * 180.0 / PI)
+
 #define PRECISION 0.000001
+typedef unsigned char byte;
 #endif /*DEFINITIONS_H*/
